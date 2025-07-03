98c85465bf36748adfc97a2317299deaff22b8a9
Ls <110120943+14solu@users.noreply.github.com>
2025-07-03 16:11:38 -0300

    .

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ let perguntaAtual;
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaPergunta.textContent = perguntaAtual.enunciado;
 }
 
 mostraPergunta();
